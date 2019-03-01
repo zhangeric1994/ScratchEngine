af76cc0f5618e23432633d11da96b6345a05fb18
Yijun Wu (RIT Student) <yw2578@ad.rit.edu>
2019-03-01 14:01:37 -0500

    Implement starter code

--- src/Editor/ScratchEngineEditor/Render/Game.cpp
+++ src/Editor/ScratchEngineEditor/Render/Game.cpp
@@ -3,11 +3,34 @@
 using namespace DirectX;
 
 Game::Game(HINSTANCE hInstance, char* name) : DXCore(hInstance, name, 1280, 720, true) {
+	vertexShader = 0;
+	pixelShader = 0;
 
+	mesh = NULL;
+
+	entityVector.resize(1);
+	for (int countOfVector = 0; countOfVector < entityVector.size(); countOfVector++)
+		entityVector[countOfVector] = NULL;
+
+	camera = new Camera();
+
+	simpleMaterial = NULL;
+
+	directionalLight.AmbientColor = XMFLOAT4(0.3f, 0.3f, 0.3f, 1.0f);
+	directionalLight.DiffuseColor = XMFLOAT4(0, 0, 0.5, 1);
+	directionalLight.Direction = XMFLOAT3(1, -1, 0);
 }
 
 Game::~Game() {
+	for (int countOfEntity = 0; countOfEntity < entityVector.size(); countOfEntity++) {
+		delete entityVector[countOfEntity];
+	}
 
+	delete vertexShader;
+	delete pixelShader;
+	if (mesh) delete mesh;
+	delete camera;
+	if (simpleMaterial) delete simpleMaterial;
 }
 
 void Game::Init() {
@@ -45,4 +68,61 @@ void Game::Draw(float deltaTime, float totalTime) {
 	//End of rendering one frame
 	swapChain->Present(0, 0);
 }
+
+#pragma region Mouse Input
+
+// --------------------------------------------------------
+// Helper method for mouse clicking.  We get this information
+// from the OS-level messages anyway, so these helpers have
+// been created to provide basic mouse input if you want it.
+// --------------------------------------------------------
+void Game::OnMouseDown(WPARAM buttonState, int x, int y) {
+	// Add any custom code here...
+
+	// Save the previous mouse position, so we have it for the future
+	prevMousePos.x = x;
+	prevMousePos.y = y;
+
+	// Caputure the mouse so we keep getting mouse move
+	// events even if the mouse leaves the window.  we'll be
+	// releasing the capture once a mouse button is released
+	SetCapture(hWnd);
+}
+
+// --------------------------------------------------------
+// Helper method for mouse release
+// --------------------------------------------------------
+void Game::OnMouseUp(WPARAM buttonState, int x, int y) {
+	// Add any custom code here...
+
+	// We don't care about the tracking the cursor outside
+	// the window anymore (we're not dragging if the mouse is up)
+	ReleaseCapture();
+}
+
+// --------------------------------------------------------
+// Helper method for mouse movement.  We only get this message
+// if the mouse is currently over the window, or if we're 
+// currently capturing the mouse.
+// --------------------------------------------------------
+void Game::OnMouseMove(WPARAM buttonState, int x, int y) {
+	// Add any custom code here...
+	if (buttonState & 0x0001) {
+		camera->SetRotationX((y - prevMousePos.y) * 0.001f);
+		camera->SetRotationY((x - prevMousePos.x) * 0.001f);
+	}
+	// Save the previous mouse position, so we have it for the future
+	prevMousePos.x = x;
+	prevMousePos.y = y;
+}
+
+// --------------------------------------------------------
+// Helper method for mouse wheel scrolling.  
+// WheelDelta may be positive or negative, depending 
+// on the direction of the scroll
+// --------------------------------------------------------
+void Game::OnMouseWheel(float wheelDelta, int x, int y) {
+	// Add any custom code here...
+}
+#pragma endregion
 
--- src/Editor/ScratchEngineEditor/Render/Game.h
+++ src/Editor/ScratchEngineEditor/Render/Game.h
@@ -3,6 +3,13 @@
 #include "DXCore.h"
 #include <DirectXMath.h>
 #include <vector>
+#include "SimpleShader.h"
+#include "Mesh.h"
+#include "Entity.h"
+#include "Camera.h"
+#include "Material.h"
+#include "Lights.h"
+#include "Vertex.h"
 
 class Game
 	: public DXCore
@@ -12,10 +19,38 @@ public:
 	~Game();
 
 	void Init();
+	void OnResize();
 	void Update(float deltaTime, float totalTime);
 	void Draw(float deltaTime, float totalTime);
 
+	void OnMouseDown(WPARAM buttonState, int x, int y);
+	void OnMouseUp(WPARAM buttonState, int x, int y);
+	void OnMouseMove(WPARAM buttonState, int x, int y);
+	void OnMouseWheel(float wheelDelta, int x, int y);
+
 private:
+
+	void LoadShaders();
+	void CreateMatrces();
+	void CreateBasicGeometry();
+
+	SimpleVertexShader* vertexShader;
+	SimplePixelShader* pixelShader;
+
+	XMFLOAT4X4 worldMatrix;
+	XMFLOAT4X4 viewMatrix;
+	XMFLOAT4X4 projectionMatrix;
+
+	Mesh* mesh;
+
+	std::vector<Entity*> entityVector;
+
+	Camera* camera;
+
+	Material* simpleMaterial;
+
+	DirectionalLight directionalLight;
+
 
 
 };
