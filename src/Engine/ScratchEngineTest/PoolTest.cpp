6db8305cf25a26634e902b1cb78da4807a790f1b
Zheyu Zhang <eric.zhangzheyu@gmail.com>
2019-03-09 18:31:58 -0500

    Added namespace reference in PoolTest.cpp

--- src/Engine/ScratchEngineTest/PoolTest.cpp
+++ src/Engine/ScratchEngineTest/PoolTest.cpp
@@ -5,6 +5,7 @@
 #include "Memory/SimplePool.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace ScratchEngine;
 using namespace ScratchEngine::Memory;
 
 namespace ScratchEngineTest
