d17d7ec49306390084cba72fa72a174fe439e536
Ollyting Xin (RIT Student) <ox7313@ad.rit.edu>
2019-03-28 12:30:46 -0400

    update

--- src/Engine/ScratchEngine/InputManager.cpp
+++ src/Engine/ScratchEngine/InputManager.cpp
@@ -12,6 +12,15 @@ bool InputManager::IsKeyDown(char key)
 	return mem[key];
 }
 
+bool InputManager::IsKeyDown(PBYTE keyCode)
+{
+	if (this->mapOfKeyCodes[keyCode] == 0) 
+	{
+		bool isDown = GetKeyboardState(keyCode);
+		this->mapOfKeyCodes[keyCode] = isDown ? 1 : -1;
+	}
+	return mapOfKeyCodes[keyCode];
+}
 bool InputManager::SetKeyState(char key, bool isDown)
 {
 	mem[key] = isDown;
@@ -26,11 +35,19 @@ void InputManager::OnMouseDown(WPARAM buttonState, int x, int y)
 {
 	this->mPos.x = x;
 	this->mPos.y = y;
+	SetCapture(this->hWnd);
 }
 
 void InputManager::OnMouseUp(WPARAM buttonState, int x, int y)
 {
-	this->mPos.x = NULL;
-	this->mPos.y = NULL;
+	//this->mPos.x = NULL;
+	//this->mPos.y = NULL;
+	ReleaseCapture();
+}
+
+void InputManager::OnMouseDrag(WPARAM buttonState)
+{
+	this->OnMouseDown(buttonState, mPos.x, mPos.y);
+	DragDetect(hWnd, mPos);
 }
 
--- src/Engine/ScratchEngine/InputManager.h
+++ src/Engine/ScratchEngine/InputManager.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <map>
 #include "windows.h"
 
 namespace ScratchEngine 
@@ -7,16 +7,20 @@ namespace ScratchEngine
 	class __declspec(dllexport) InputManager
 	{
 	private:
+		HWND hWnd;
 		char* mem;
+		std::map<PBYTE, bool> mapOfKeyCodes;
 		POINT mPos;
 	public:
 		// keyboard input
 		bool IsKeyDown(char);
+		bool IsKeyDown(PBYTE);
 		bool SetKeyState(char, bool);
 		void Clear();
 
 		// mouse input
 		void OnMouseDown(WPARAM, int, int);
 		void OnMouseUp(WPARAM, int, int);
+		void OnMouseDrag(WPARAM);
 	};
 }
